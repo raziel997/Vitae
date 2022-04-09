8238b77443446266ead7ed1e636413c32b1ffd3a
Elihu Solis <elihu997@gmail.com>
2022-04-09 16:59:39 -0500

    Add Context Connection to Project

--- Services/Vitae/Vitae/Models/vitaeContext.cs
+++ Services/Vitae/Vitae/Models/vitaeContext.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.Extensions.Options;
 
 #nullable disable
 
@@ -8,14 +9,17 @@ namespace Vitae.Models
 {
     public partial class vitaeContext : DbContext
     {
+        public DBConnectionModel _connectionString { get; }
         public vitaeContext()
         {
         }
 
-        public vitaeContext(DbContextOptions<vitaeContext> options)
+        public vitaeContext(DbContextOptions<vitaeContext> options,IOptions<DBConnectionModel> connectionString)
             : base(options)
         {
+            _connectionString = connectionString.Value;
         }
+        
 
         public virtual DbSet<Address> Addresses { get; set; }
         public virtual DbSet<City> Cities { get; set; }
@@ -32,8 +36,7 @@ namespace Vitae.Models
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=localhost,14033;Database=vitae;User=sa;Password=Passw0rd;");
+                optionsBuilder.UseSqlServer(_connectionString.Connection);
             }
         }
 
