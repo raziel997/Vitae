3f32dc4e6ef23cbbc63301a69a6b574f150b1d11
Elihu Solis <elihu997@gmail.com>
2022-04-12 01:01:31 -0500

    Add response model and Repository

---
+++ Services/Vitae/Vitae/Helpers/Repository/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace Vitae.Helpers.Repository
+{
+    public interface IRepository<T>
+    {
+        IEnumerable<T> Get();
+        T Get(int Oid);
+        void Add(T item);
+        void Delete(int Oid);
+        void Update(T item);
+        void Save();
+    }
+}
---
+++ Services/Vitae/Vitae/ViewModel/Responses/Response.cs
@@ -0,0 +1,10 @@
+﻿using Vitae.Models;
+
+namespace Vitae.ViewModel.Responses
+{
+    public class Response<T> 
+    {
+        public T Data { get; set; }
+        public string Error { get; set; }
+    }
+}
